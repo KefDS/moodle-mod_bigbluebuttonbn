e5d1eced560e41d26c2e48308d5820697b7bb007
Carlos Mata <carlos.mataguzman@ucr.ac.cr>
2016-10-27 19:51:19 -0600

    Prubeas server url

--- bbb_broker.php
+++ bbb_broker.php
@@ -30,10 +30,15 @@ $opensack_username = bigbluebuttonbn_get_cfg_openstack_username();
 $openstack_password = bigbluebuttonbn_get_cfg_openstack_password();
 $openstack_tenant_id =bigbluebuttonbn_get_cfg_openstack_tenant_id();
 
+
+$openStack_integration_enabled = bigbluebuttonbn_get_cfg_openstack_integration();
+$meetingid = substr($params['id'], 0, strpos($params['id'], '-'));
+
+$endpoint = ($openStack_integration_enabled)? bigbluebuttonbn_get_meeting_server_url($meetingid): bigbluebuttonbn_get_cfg_server_url();
+$shared_secret = ($openStack_integration_enabled)? bigbluebuttonbn_get_meeting_shared_secret($meetingid): bigbluebuttonbn_get_cfg_shared_secret();
+
 //---- end of OpenStack integration ----
 
-$endpoint = bigbluebuttonbn_get_cfg_server_url();
-$shared_secret = bigbluebuttonbn_get_cfg_shared_secret();
 
 $error = '';
 
